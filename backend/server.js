47601200d4bd361ab113f1f2c432da7403b3347a
Kelvin Bruce <brucekelvin742@gmail.com>
2025-08-19 17:57:12 +0000

    removed the port

--- backend/server.js
+++ backend/server.js
@@ -74,7 +74,6 @@ const db = mysql.createConnection({
     user: process.env.DB_USER,  
     password: process.env.DB_PASSWORD, 
     database: process.env.DB_NAME,
-    port: process.env.PORT || 3001
 });
 
 // Testing my connection
